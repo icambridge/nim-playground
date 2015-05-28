2441805302db64d622d00bb3b1dbc31666abe11e
Iain Cambridge <i.cambridge@foodpanda.com>
2015-05-28 21:18:43 +0200

    Basic unit testing

---
+++ src/helloworld.nim
@@ -0,0 +1,3 @@
+
+proc helloworlds*: string =
+  return "helloworld"
---
+++ tests/hello.nim
@@ -0,0 +1,5 @@
+import unittest, helloworld
+
+suite "helloworld ":
+  test " helloworld 2":
+    check(helloworlds() == "helloworld")
